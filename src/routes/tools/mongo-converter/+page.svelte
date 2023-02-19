f163b374077d5b7213b07b138351dfe2fc72163a
insuk <cis1725@gmail.com>
2023-02-19 21:59:52 +0900

    mongo-converter page init

--- src/routes/tools/mongo-converter/+page.svelte
+++ src/routes/tools/mongo-converter/+page.svelte
@@ -1,1 +1,24 @@
-Hi heera
+<script>
+    import { Textarea, P, Button, Label } from 'flowbite-svelte'
+    // import SQLMongoParser from './sql-to-mongo-wrapper.js';
+
+    let mysqlQuery;
+    let mongoQuery;
+
+    const handleSubmit = () => {
+        mongoQuery = mysqlQuery;
+    };
+</script>
+<br>
+<P size="3xl">Mysql Query</P>
+<form on:submit|preventDefault={handleSubmit}>
+  <Textarea class="mb-4" placeholder="Write a MongoQuery" bind:value={mysqlQuery}>
+    <div slot="footer" class="flex items-center justify-between">
+        <Button type="submit">Convert Mongo Query</Button>
+    </div>
+  </Textarea>
+</form>
+<br>
+
+<P size="3xl">Mongo Query</P>
+<Textarea id="textarea-id" placeholder="..." rows="4" bind:value="{mongoQuery}" readonly/>
