615a5dcdb02aea14a4ecb9f6c87e2e1a8159e0a3
insuk <cis1725@gmail.com>
2023-02-23 22:11:53 +0900

    query or aggregation 분기처리

--- src/routes/api/convertToMongo/+server.ts
+++ src/routes/api/convertToMongo/+server.ts
@@ -4,8 +4,9 @@ import { type RequestHandler } from '@sveltejs/kit'
 export const POST: RequestHandler = async (event) => {
     const data = await event.request.formData()
     const mysqlQuery = data.get('mysqlText');
-    const mongoQuery = SQLMongoParser.parseSQL(mysqlQuery)
-    // const mongoQuery = SQLMongoParser.parseSQL("select id from films")
+    const mongoQuery = SQLMongoParser.canQuery(mysqlQuery) ?
+        SQLMongoParser.makeMongoQuery(mysqlQuery) :
+        SQLMongoParser.makeMongoAggregate(mysqlQuery);
 
     return new Response(JSON.stringify({ mongoQuery: mongoQuery }), {
         headers: {
